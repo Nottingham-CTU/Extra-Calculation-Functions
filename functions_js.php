6074d31f6175d3f109ca23063eb5663a84442fda
Richard Dooley <richard.dooley@nottingham.ac.uk>
2021-08-11 08:23:18 +0100

    Amended datalookup javascript function to perform the lookup asynchronously.

--- functions_js.php
+++ functions_js.php
@@ -40,34 +40,42 @@ __halt_compiler();
 
 // datalookup: retrieve data from any REDCap project
 
-function datalookup()
+datalookup = (function()
 {
-	if ( arguments.length < 1 )
+	var luCache = {}
+	return function ()
 	{
-		return ''
+		if ( arguments.length < 1 )
+		{
+			return ''
+		}
+		var luName = arguments[0]
+		var luArgs = []
+		for ( var i = 1; i < arguments.length; i++ )
+		{
+			luArgs.push( arguments[i] )
+		}
+		luArgs = JSON.stringify( luArgs )
+		if ( luCache[ luName ] == undefined )
+		{
+			luCache[ luName ] = {}
+		}
+		if ( luCache[ luName ][ luArgs ] == undefined )
+		{
+			$.ajax( { url : 'datalookup.php',
+			          method : 'POST', headers : { 'X-RC-ECF-Req' : '1' },
+			          dataType : 'json', data : { name : luName, args : luArgs },
+			          success : function ( result )
+			          {
+			            luCache[ luName ][ luArgs ] = result
+			            calculate()
+			          }
+			        } )
+			return ''
+		}
+		return luCache[ luName ][ luArgs ]
 	}
-	var luName = arguments[0]
-	var luArgs = []
-	for ( var i = 1; i < arguments.length; i++ )
-	{
-		luArgs.push( arguments[i] )
-	}
-	var luResult = ''
-	$.ajax( { url : 'datalookup.php',
-	          method : 'POST',
-	          data : { name : luName,
-	                   args : JSON.stringify(luArgs) },
-	          headers : { 'X-RC-ECF-Req' : '1' },
-	          dataType : 'json',
-	          success : function ( result )
-	          {
-	            luResult = result
-	          },
-	          async : false,
-	          timeout : 10000
-	        } )
-	return luResult
-}
+})()
 
 
 
